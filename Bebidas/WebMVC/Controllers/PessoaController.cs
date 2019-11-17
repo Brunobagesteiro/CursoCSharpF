4de1083bff32b8a8f685b5595c8dd9f58f981bf9
Bruno T. Bagesteiro <brunobagesteiro1@hotmail.com>
2019-11-17 19:03:35 -0300

    Aulo final

---
+++ Bebidas/WebMVC/Controllers/PessoaController.cs
@@ -0,0 +1,44 @@
+﻿using Dados.DAO;
+using Dados.Modelo;
+using Dados.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class PessoaController : Controller
+    {
+        PessoaRepositorio repositorio = new PessoaRepositorio();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Pessoa model)
+        {
+           
+            repositorio.Salvar(model);
+            ViewBag.Mensagem = $"A pessoa {model.Nome} foi salva com Sucesso";
+            ModelState.Clear();
+            return View();
+
+        }
+
+        public ActionResult List()
+        {
+
+            List<Pessoa> listaPessoas = repositorio.ListarTodos();
+
+            return View (listaPessoas);
+        }
+    }
+}
